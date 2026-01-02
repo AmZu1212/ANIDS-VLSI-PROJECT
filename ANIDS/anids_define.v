80ef0bc554f121b9580d6ab151e108af7c45ee38
Amir Zuabi <zuabiamir123@gmail.com>
2026-01-02 15:49:56 +0200

    updated definitions, testbench & data file formats

--- ANIDS/anids_define.v
+++ ANIDS/anids_define.v
@@ -1,39 +1,51 @@
-/* ident "@(#)tso_define.v:/dtv/i68/project/design/hdl/ifu/tso/SCCS/s.tso_define.v 1.14 04/06/03" */
-/**************************************************************************/
-/*                                                                        */
-/*    Copyright (c) 1998 ZORAN Corporation, All Rights Reserved           */
-/*    THIS IS UNPUBLISHED PROPRIETARY SOURCE CODE OF ZORAN CORPORATION    */
-/*                                                                        */
-/*
- *    File : SCCS/s.tso_define.v
- *    Type : Verilog File
- *    Module : tso_define.v
- *    Sccs Identification (SID) : 1.14
- *    Modification Time : 04/06/03 14:17:02
- *                                                                        */
-/*                                                                        */
-/**************************************************************************/
+// ANIDS Top Level Defines
+// APB can work with 8,16,32 bit data width
+`define APB_DATA_WIDTH	8
 
-// tso Registers address.
-`define TSO_Control   13'h0970
-`define TSO_In_cfg    13'h0971
-`define TSO_Out_cfg   13'h0972
-`define TSO_Clk_cfg   13'h0973
-`define TSO_Status    13'h0974
-`define TSO_Pkt_len   13'h0975
-`define TSO_Pkt_count 13'h0976
+// DMA Data Width is input vector size, which is 128 bits
+`define DMA_DATA_WIDTH	128
 
+// ANIDS Control Register Addresses
+`define START_REG		16'h0000;
+`define N_REG			16'h0001;
+`define THRESHOLD_REG	16'h0002;
+`define RESULT_REG		16'h0003;
+`define CTRL1_REG		16'h0004;
+`define CTRL2_REG		16'h0005;
+`define CTRL3_REG		16'h0006;
+`define CTRL4_REG		16'h0007;
 
-// Transport Clock Generator
-`define TSCLK_PERIOD  12'd40
+// Hidden Layer Memory Mapping
+`define W_BASE			16'h0008; // 64*128 bytes: 	0x0008 -> 0x2007
+`define HL_BIAS_BASE	16'h2008; // 64 bytes:    	0x2008 -> 0x2047
 
+// Output Layer Memory Mapping
+`define O_BASE			16'h2048; // 128*64 bytes:  0x2048 -> 0x4047
+`define OL_BIAS_BASE	16'h4048; // 128 	bytes:  0x4048 -> 0x40C7
 
-// TSO Packet Size.
-`define PACKET_SIZE 8'd187
-
-// TSO Buffer FIFO ram
-`define TSO_RAM_SIZE 6'd48
+// Clock Generator (not sure if needed) this is 5ns period (200MHz clock)
+`define CLK_PERIOD		16'd5
 
 
-// tso Synchronizing Byte.
-// `define SYNC_BYTE   8'h47         //  moved to i88_define
+// Pipeline Cycle Definitions
+`define INPUT_LAYER_CYCLES 		64
+`define HIDDEN_LAYER_CYCLES 	64
+`define OUTPUT_LAYER_CYCLES 	64
+`define LOSS_FUNCTION_CYCLES 	64
+`define OUTLIER_CYCLES 			1
+`define RESULT_CYCLES 			1
+`define FILL_CYCLES (
+					`INPUT_LAYER_CYCLES 	+
+					`HIDDEN_LAYER_CYCLES 	+
+					`OUTPUT_LAYER_CYCLES 	+
+					`LOSS_FUNCTION_CYCLES 	+
+					`OUTLIER_CYCLES 		+
+					`RESULT_CYCLES )	 // = 259
+
+`define LATENCY_IN_CYCLES (
+					`LOSS_FUNCTION_CYCLES 	+
+					`OUTLIER_CYCLES 		+
+					`RESULT_CYCLES ) 	 // = 66
+
+
+
--- ANIDS/anids_tb.v
+++ ANIDS/anids_tb.v
@@ -1,476 +1,292 @@
-/* ident "@(#)tso_tb.v:/dtv/i88/project/design/ifu/tso/tso_tb.v 1.17 06/25/07" */
-/**************************************************************************/
-/*                                                                        */
-/*    Copyright (c) 1998 ZORAN Corporation, All Rights Reserved           */
-/*    THIS IS UNPUBLISHED PROPRIETARY SOURCE CODE OF ZORAN CORPORATION    */
-/*                                                                        */
-/*
- *    File : tso_tb.v
- *    Type : Verilog File
- *    Module : tso_tb.v
- *    Modification Time : 06/25/07 15:28:09
- *                                                                        */
-/*                                                                        */
-/**************************************************************************/
+// ANIDS - Test Bench
+`define CPU_BUS_DATA		"model.apb"
+`define DMA_DATA			"dma.data"
+`define NUM_RESULTS			1
+`timescale 1ns/100ps 		// period of 5ns (200MHz)
 
 
-
-/*************************************************************************************
-
-  TSO Test-Bench
-
-*************************************************************************************/
-
-// The TB tests the TSO interface with the CPU, IO, tso and MCU.
-
-
-
-`define TSI_TSO_DATA       "tsi_tso_data.tso"
-`define MCU_TSO_DATA       "mcu_tso_data.tso"
-`define CPU_CBUS_DATA      "cpu_cbus_data.tso"
-`define NUM_OF_WORDS       60
-
-`timescale 1ns/100ps
-
-module tso_tb;
-
-
+module anids_tb;
 // -----------------------------------------------------------
 //                  Registers & Wires
 // -----------------------------------------------------------
 
-   wire [7:0]   tsi1_data_in ;
-   wire         tsi1_wr;
-   wire 	tsi1_start;
+// System clock & reset
+reg sys_clk;
+reg sys_reset;
 
-   wire [31:0]  tso_cpu_obus;
-   wire		tso_cpu_rdy;
-   reg		cpu_tso_wr;
-   reg 		cpu_tso_rd;
-   reg [12:0] 	cpu_tso_address;
-   reg [31:0] 	cpu_tso_bus_in;
+// APB Interface
+reg        					PCLK;
+reg        					PRESETn;
+reg  [`APB_DATA_WIDTH-1:0] 	PADDR;
+reg  [`APB_DATA_WIDTH-1:0] 	PWDATA;
+wire [`APB_DATA_WIDTH-1:0] 	PRDATA;
+reg        					PSEL;
+reg        					PENABLE;
+reg        					PWRITE;
+wire       					PREADY;
+wire       					PSLVERR;
+reg  [3:0]  				PSTRB;
+reg  [2:0]  				PPROT;
 
-   reg 		misc_tso_clk;
-   reg 		misc_tso_rst;
-   reg 		misc_tso_scan_act;
+// DMA Interface
+reg 						dma_valid;
+wire 						dma_ack;
+reg [`DMA_DATA_WIDTH-1:0]	dma_data;
 
-   wire 	mcu_tso_wr;
-   wire [7:0] 	tso_mcu_avail;
-   wire [31:0]  mcu_tso_data;
-
-   wire 	tso_ram_cs;
-   wire 	tso_ram_rd;
-   wire [5:0] 	tso_ram_addr;
-   wire [31:0]	tso_ram_d_in;
-   wire [31:0]  ram_tso_d_out;
-
-   reg          flag_clk;
-
-   integer      i;
-
-   reg [12:0]   tmp_cpu_address;
-   reg [31:0]   tmp_cpu_data;
-   reg [7:0]    tmp_tsi1_data_in;
-
-   reg [44:0]   cpu_trans_reg [27:0];
-
-   reg          mcu_start;
-   reg          tsi_start;
-
-   wire			tso_clk_int;
-   wire	        tso_start;
-   wire	        tso_valid;
-   wire	[7:0]   tso_data;
-   wire	        tso_serial;
-   wire	        tso_pclk;
-
+// Clocks
+always #(`CLK_PERIOD/2) 	sys_clk = ~sys_clk;
+always #(`CLK_PERIOD/2) 	PCLK    = ~PCLK;
 
 
 // ----------------------------------------------------------------------
-//                   Instantiation
+//                   Instantiations
 // ----------------------------------------------------------------------
+anids dut (
+    // System signals
+    .sys_clk   (sys_clk),
+    .sys_reset (sys_reset),
 
-tso tso(   .clk			(misc_tso_clk),
-           .`A_RESET		(misc_tso_rst),
-           .scan_act		(misc_tso_scan_act),
+    // APB Interface
+    .pclk      (PCLK),
+    .presetN   (PRESETn),
+    .paddr     (PADDR),
+    .pwdata    (PWDATA),
+    .prdata    (PRDATA),
+    .psel      (PSEL),
+    .penable   (PENABLE),
+    .pwrite    (PWRITE),
+    .pready    (PREADY),
+    .pslverr   (PSLVERR),
+    .pstrb     (PSTRB),
+    .pprot     (PPROT),
 
-           .c_tso_obus		(tso_cpu_obus[31:0]),
-           .c_tso_rdy		(tso_cpu_rdy),
-           .cpu_wr		(cpu_tso_wr),
-           .cpu_rd		(cpu_tso_rd),
-           .c_add		(cpu_tso_address[12:0]),
-           .c_bus_in		(cpu_tso_bus_in[31:0]),
-
-           .tso_mcu_avail	(tso_mcu_avail[7:0]),
-           .mcu_wr		(mcu_tso_wr),
-           .mcu_d_bus_in	(mcu_tso_data[31:0]),
-
-           .tso_clk_int		(tso_clk_int),
-           .tso_start   	(tso_start),
-           .tso_valid    	(tso_valid),
-           .tso_data     	(tso_data[7:0]),
-	   .tso_serial          (tso_serial),
-	   .tso_pclk            (tso_pclk),
-
-           .tso_ram_cs	        (tso_ram_cs),
-           .tso_ram_rd	        (tso_ram_rd),
-           .tso_ram_addr	(tso_ram_addr[5:0]),
-           .tso_ram_d_in	(tso_ram_d_in[31:0]),
-           .tso_ram_d_out	(ram_tso_d_out[31:0]),
-
-           .tsi1_data_in	(tsi1_data_in[7:0]),
-           .tsi1_wr		(tsi1_wr),
-           .tsi1_start		(tsi1_start),
-           .tsi1_err		(tsi1_err),
-
-           .tsi2_data_in	(8'b0),
-           .tsi2_wr		(1'b0),
-           .tsi2_start		(1'b0),
-           .tsi2_err		(1'b0),
-
-           .fe_data_in          (8'b0),
-           .fe_start            (1'b0),
-           .fe_wr               (1'b0),
-           .fe_valid            (1'b0),
-           .fe_fail             (1'b0)
-	  );
-
-
-// New Storage-RAM (buffer) - with a retention shell
-ram48x32_w ram(   .nreset	(misc_tso_rst),
-                  .clk		(misc_tso_clk),
-                  .csn		(tso_ram_cs),
-                  .wen		(tso_ram_rd),
-                  .a		(tso_ram_addr[5:0]),
-                  .i		(tso_ram_d_in[31:0]),
-                  .o		(ram_tso_d_out[31:0]),
-                  .scan_shift	(misc_tso_scan_act),
-                  .bist_mode	(1'b0),
-                  .bist_in	(60'b0),
-                  .bist_out	(),
-                  .retention	(1'b1),
-                  .scan_ram_bypass  (1'b0),
-                  .amp_adjust	(3'b011)
-                 );
-
-
-mcu_stub mcu(  .mcu_clk           (misc_tso_clk),
-               .mcu_out_wr        (mcu_tso_wr),
-               .mcu_out_data      (mcu_tso_data[31:0]),
-               .mcu_avail         (tso_mcu_avail[7:0]),
-               .start	          (mcu_start)
-	       );
-
-
-tsi1_stub tsi1(  .tsi1_clk       (misc_tso_clk),
-                 .tsi1_data      (tsi1_data_in[7:0]),
-                 .tsi1_wr        (tsi1_wr),
-                 .tsi1_start     (tsi1_start),
-		 .tsi1_err       (tsi1_err),
-                 .start	         (tsi_start)
-	       );
+    // DMA Interface
+    .dma_valid (dma_valid),
+    .dma_ack   (dma_ack),
+    .dma_data  (dma_data)
+);
 
 
 // ----------------------------------------------------------------------
 //                   Test Pattern
 // ----------------------------------------------------------------------
-
-
-initial begin
-   initiate_all;                                 // Initiates all input signals to '0' and open necessary files
-
-   #100
-   @(posedge misc_tso_clk);
-   cpu_write_TSI;                                // CPU WRITE configurations (TSI master)
-   #100
-   @(posedge misc_tso_clk);
-   cpu_read_TSI;                                 // CPU READ transaction
-   @(posedge misc_tso_clk);
-   mcu_start = 1'b0;
-   tsi_start = 1'b1;
-
-
-   #500000                                       /* Turn-Over (TO) */
-   @(posedge misc_tso_clk);
-   flush_mem ;                                   // clear the buffer by software.
-
-
-   @(posedge misc_tso_clk);
-   cpu_write_MCU;                                // CPU WRITE configurations (MCU master)
-   #100
-   @(posedge misc_tso_clk);
-   cpu_read_MCU;                                 // CPU READ transaction
-   @(posedge misc_tso_clk);
-   tsi_start = 1'b0;
-   mcu_start = 1'b1;
-
-
-//   #100000  $finish;
-
-end
-
-
-
-
-// ----------------------------------------------------------------------
-//                   Clock generator  (Duty cycle 5ns)
-// ----------------------------------------------------------------------
-
-
-  always
-    #2.5 misc_tso_clk = ~misc_tso_clk;
-
-
-// ----------------------------------------------------------------------
-//                   Tasks
-// ----------------------------------------------------------------------
-
-
- task initiate_all;        // sets all tso inputs to '0'.
-      begin
-	 misc_tso_clk = 1'b0;
-	 misc_tso_rst = 1'b0;
-	 misc_tso_scan_act = 1'b0;
-
-	 cpu_tso_wr = 1'b0;
-	 cpu_tso_rd = 1'b0;
-	 cpu_tso_address = 13'b0;
-	 cpu_tso_bus_in = 32'b0;
-
-	 mcu_start = 1'b0;
-	 tsi_start = 1'b0;
-
-	 flag_clk = 0;
-
-         #2 misc_tso_rst = 1'b1;     // Disable Reset signal.
-
-         $readmemh(`CPU_CBUS_DATA, cpu_trans_reg);      // opens the cpu configuration file.
-      end
- endtask
-
-
- task cpu_write_TSI();
-   for (i=0; i<7; i=i+1) begin
-   cpu_trans_reg[i][44:0] = cpu_trans_reg[i];
-     if (cpu_trans_reg[i][44] == 1) begin
-
-         tmp_cpu_address = cpu_trans_reg[i][43:32];
-         tmp_cpu_data    = cpu_trans_reg[i][31:0];
-         @(posedge misc_tso_clk);
-	 cpu_tso_wr       = 1'b1;
-	 cpu_tso_rd       = 1'b0;
-	 cpu_tso_address  = tmp_cpu_address;
- 	 cpu_tso_bus_in   = tmp_cpu_data;
-	 @(posedge misc_tso_clk);
-	 #1 cpu_tso_wr    = 1'b0;
-	 cpu_tso_rd       = 1'b0;
-	 cpu_tso_address  = 13'h0;
- 	 cpu_tso_bus_in   = 31'h0;
-        end
-     else $display("Error reading data from input file !\n\n");
-   end
- endtask
-
-
- task cpu_read_TSI;
-   for (i=0; i<7; i=i+1) begin
-     if (cpu_trans_reg[i+7][44] == 0) begin
-        tmp_cpu_address = cpu_trans_reg[i+7][43:32];
-        @(posedge misc_tso_clk);
-	cpu_tso_wr    = 1'b0;
-	cpu_tso_rd     = 1'b1;
-	cpu_tso_address  = tmp_cpu_address;
-	@(posedge misc_tso_clk);
-	#1 cpu_tso_wr    = 1'b0;
-	cpu_tso_rd     = 1'b0;
-	cpu_tso_address  = 13'h0;
-       end
-     else $display("Error reading data from input file !\n\n");
-   end
- endtask
-
-  task cpu_write_MCU();
-   for (i=14; i<21; i=i+1) begin
-   cpu_trans_reg[i][44:0] = cpu_trans_reg[i];
-     if (cpu_trans_reg[i][44] == 1) begin
-
-         tmp_cpu_address = cpu_trans_reg[i][43:32];
-         tmp_cpu_data    = cpu_trans_reg[i][31:0];
-         @(posedge misc_tso_clk);
-	 cpu_tso_wr       = 1'b1;
-	 cpu_tso_rd       = 1'b0;
-	 cpu_tso_address  = tmp_cpu_address;
- 	 cpu_tso_bus_in   = tmp_cpu_data;
-	 @(posedge misc_tso_clk);
-	 #1 cpu_tso_wr    = 1'b0;
-	 cpu_tso_rd       = 1'b0;
-	 cpu_tso_address  = 13'h0;
- 	 cpu_tso_bus_in   = 31'h0;
-        end
-     else $display("Error reading data from input file !\n\n");
-   end
- endtask
-
-
- task cpu_read_MCU;
-   for (i=14; i<21; i=i+1) begin
-     if (cpu_trans_reg[i+7][44] == 0) begin
-        tmp_cpu_address = cpu_trans_reg[i+7][43:32];
-        @(posedge misc_tso_clk);
-	cpu_tso_wr    = 1'b0;
-	cpu_tso_rd     = 1'b1;
-	cpu_tso_address  = tmp_cpu_address;
-	@(posedge misc_tso_clk);
-	#1 cpu_tso_wr    = 1'b0;
-	cpu_tso_rd     = 1'b0;
-	cpu_tso_address  = 13'h0;
-       end
-     else $display("Error reading data from input file !\n\n");
-   end
- endtask
-
-
- task flush_mem;
- begin
-   cpu_tso_wr       = 1'b1;
-   cpu_tso_rd       = 1'b0;
-   cpu_tso_address  = `TSO_In_cfg;
-   if (mcu_start)
-      cpu_tso_bus_in   = 32'h00000006;              // FFClear = 1
-   else
-      cpu_tso_bus_in   = 32'h00000004;
-   @(posedge misc_tso_clk);
-   #1 cpu_tso_wr    = 1'b0;
-   cpu_tso_rd       = 1'b0;
-   cpu_tso_address  = 13'h0;
-   cpu_tso_bus_in   = 31'h0;
- end
- endtask
-
-
-endmodule   // tso_tb
-
-
-
-// ----------------------------------------------------------------------
-//                   MCU Stub
-// ----------------------------------------------------------------------
-
-
-module mcu_stub(mcu_out_wr, mcu_out_data, mcu_avail, mcu_clk, start);
-
- output 	 mcu_out_wr;
- output [31:0]	 mcu_out_data;
- input  [7:0]    mcu_avail;
- input           mcu_clk;
- input           start;
-
- reg             mcu_out_wr;
- reg    [31:0]   mcu_out_data;
- reg    [31:0]   mcu_dout;
- reg    [31:0]   mcu_trans_reg [4*(`NUM_OF_WORDS)-1:0];
-
- integer         i;
-
+byte result;
+reg [`APB_DATA_WIDTH-1:0] rdata;
 
 initial begin
-  i = 0;
-  mcu_out_wr = 1'b0;
-  mcu_out_data = 32'b0;
-  $readmemh(`MCU_TSO_DATA, mcu_trans_reg);                                           // opens the MCU data file
+	// Initiates all input signals
+	initiate_all;
+	#10*`CLK_PERIOD;
+
+	// Load the model to ANIDS
+	load_model(`CPU_BUS_DATA);
+	//#`CLK_PERIOD * apb write delay * number of writes (not sure if needed...)
+
+
+	/// TODO: check with shahar that this is correct behaviour
+	// Start DMA data stream from file (async)
+	fork
+		dma_stream_from_file("dma.data");
+	join_none
+	#4*`CLK_PERIOD;
+
+
+	// Start ANIDS processing by writing to START_REG
+	cpu_write_APB(`START_REG, `APB_DATA_WIDTH'h1);
+	$display("ANIDS processing started.");
+
+	// first read is always slowest due to pipeline fill
+	#`FILL_CYCLES * `CLK_PERIOD;
+
+	/// TODO: verify syntax & logic for this loop
+	// Poll RESULT_REG until done
+	integer k;
+	for (k = 0; k < `NUM_RESULTS; k++)
+	begin
+		cpu_read_APB(`RESULT_REG, rdata);
+		result = rdata[`APB_DATA_WIDTH-1:0];
+		$display("Result[%0d] = 0x%02h at t=%0t", k, result, $time);
+
+		// spacing between results (adjust accordingly)
+		#(`LATENCY_IN_CYCLES * `CLK_PERIOD);
+	end
+
+	// Stop ANIDS & end TB
+	cpu_write_APB(`START_REG, `APB_DATA_WIDTH'h0);
+	$display("ANIDS processing stopped.");
+	$display("Test Bench completed.");
+	$finish; // also kills the forked process
 end
 
-
- always @(posedge mcu_clk)
-   if (start && (i < 4*`NUM_OF_WORDS) && (mcu_avail > 8'b0))
-     begin
-       mcu_dout = mcu_trans_reg[i][31:0];
-       i = i+1;
-     end
-
- always @(posedge mcu_clk)
-   if (start && (mcu_avail > 8'b0))   mcu_write(mcu_dout);
+// TASKS
+/*
+ * 	APB RULE OF THUMB:
+ *	| WRITE: CPU configures, loads or starts the DUT.
+ *	|  READ:  CPU checks status or reads results.
+ */
 
 
- task mcu_write;
-   input  [31:0]    mcu_tso_data;
-      begin
-           mcu_out_wr = 1'b1;
-        #1 mcu_out_data = mcu_tso_data;
-      end
- endtask
+/// TODO: maybe add streaming
+// currently doesnt support (strb? i.e data stream)
+task cpu_write_APB(
+	input [`APB_DATA_WIDTH-1:0] addr,
+	input [`APB_DATA_WIDTH-1:0] data
+	);
+	begin
+		// SETUP phase
+		@(posedge PCLK);
+		PSEL   <= 1'b1;
+		PENABLE<= 1'b0;
+		PWRITE <= 1'b1;
+		PADDR  <= addr;
+		PWDATA <= data;
 
-endmodule  // mcu_stub
+		// ACCESS phase
+		@(posedge PCLK);
+		PENABLE <= 1'b1;
 
+		// Wait for slave ready
+		while (!PREADY)
+			@(posedge PCLK);
 
-// ----------------------------------------------------------------------
-//                   TSI1 Stub
-// ----------------------------------------------------------------------
+		// End transfer
+		@(posedge PCLK);
+		PSEL    <= 1'b0;
+		PENABLE <= 1'b0;
+		PWRITE  <= 1'b0;
+	end
+endtask
 
+task cpu_read_APB(
+	input  [`APB_DATA_WIDTH-1:0] addr,
+	output [`APB_DATA_WIDTH-1:0] data
+	);
+	begin
+		// SETUP phase
+		@(posedge PCLK);
+		PSEL    <= 1'b1;
+		PENABLE <= 1'b0;
+		PWRITE  <= 1'b0;
+		PADDR   <= addr;
 
-module tsi1_stub(tsi1_clk, tsi1_wr, tsi1_data, tsi1_start, tsi1_err, start);
+		// ACCESS phase
+		@(posedge PCLK);
+		PENABLE <= 1'b1;
 
- output 	 	 tsi1_wr;
- output [7:0]	 tsi1_data;
- output          tsi1_start;
- output          tsi1_err;
- input           tsi1_clk;
- input           start;
+		// Wait for slave ready
+		while (!PREADY)
+			@(posedge PCLK);
 
- reg 	         tsi1_wr;
- reg    [7:0]	 tsi1_data;
- reg             tsi1_start;
- reg             tsi1_err;
- reg             init_start;
+		// Capture read data
+		data = PRDATA;
 
- reg    [7:0]    tsi_trans_reg [4*(`NUM_OF_WORDS)-1:0];
+		// End transfer
+		@(posedge PCLK);
+		PSEL    <= 1'b0;
+		PENABLE <= 1'b0;
+	end
+endtask
 
- integer 	 	 i;
- integer         pack_len_count;
+/// TODO: verify this is the actual values that are supposed to be here
+task initiate_all;
+	begin
+		// Initialize all input signals
+		sys_clk    = 1'b0;
+		sys_reset  = 1'b1;
 
+		PCLK       = 1'b0;
+		PRESETn    = 1'b0;
+		PADDR      = `APB_DATA_WIDTH'b0;
+		PWDATA     = `APB_DATA_WIDTH'b0;
+		PSEL       = 1'b0;
+		PENABLE    = 1'b0;
+		PWRITE     = 1'b0;
+		PSTRB      = 4'b1111; // all byte lanes active
+		PPROT      = 3'b000;  // data access, privileged, secure
 
- initial begin
-   i = 0;
-   pack_len_count = 0;
-   init_start = 1'b0;
-   tsi1_wr = 1'b0;
-   tsi1_start = 1'b0;
-   $readmemh(`TSI_TSO_DATA, tsi_trans_reg);                                          // opens the TSI data file
- end
+		dma_valid  = 1'b0;
+		dma_data   = `DMA_DATA_WIDTH'b0;
 
+		// Release reset after some time
+		#20;
+		sys_reset  = 1'b0;
+		PRESETn    = 1'b1;
+	end
+endtask
 
- always @(posedge tsi1_clk)
-   if (tsi1_start)	init_start <= #1 1'b1;                                       // waking up after Reset.
+// task for loading the model values from a file
+task automatic load_model(input string fname);
+	integer fd;
+	string line;
+	int unsigned addr;
+	int unsigned val;
 
- always @(posedge tsi1_clk)
-   if (start)            tsi_write(tsi1_data);
+	fd = $fopen(fname, "r");
+	if (fd == 0) $fatal(1, "Cannot open %s", fname);
 
- always @(posedge tsi1_clk)
-   if (start && (tsi1_data==`SYNC_BYTE)) begin
-      tsi1_wr  =  1'b1;
-      tsi1_start = init_start ? (pack_len_count==187) : 1'b1;
-      @(posedge tsi1_clk);
-      tsi1_start = 1'b0;
-      end
+	while (!$feof(fd)) begin
+		line = "";
+		void'($fgets(line, fd));
 
- always @(posedge tsi1_clk)
-   if (start && (i < 4*`NUM_OF_WORDS))  begin
-      tsi1_data = tsi_trans_reg[i][7:0];
-      i = i+1;
-      end
+		// skip comment/blank
+		if (line.len() == 0) continue;
+		if (line.len() >= 2 && line.substr(0,1) == "//") continue;
 
- always @(posedge tsi1_clk)
-      pack_len_count = tsi1_start ? 0 : pack_len_count + 1;
+		// strip inline comment
+		int cpos = line.find("//");
+		if (cpos != -1) line = line.substr(0, cpos - 1);
 
+		// parse: 0xADDR 0xVAL
+		if ($sscanf(line, "0x%x 0x%x", addr, val) == 2) begin
+		cpu_write_APB(addr, val[`APB_DATA_WIDTH-1:0]);
+		end
+	end
 
- task tsi_write;
-   input [7:0]     tsw_data;
-      begin
-         tsi1_data = tsw_data;
-         tsi1_err = 1'b0;
-      end
- endtask
+	$fclose(fd);
+endtask
 
-endmodule  // tsi1_stub
+/// Async DMA loading task
+task automatic dma_stream_from_file(input string fname);
+	integer fd;
+	reg [`DMA_DATA_WIDTH-1:0] vec;
 
+	// idle defaults
+	dma_valid <= 1'b0;
+	dma_data  <= {`DMA_DATA_WIDTH{1'b0}};
+
+	fd = $fopen(fname, "r");
+	if (fd == 0)
+	$fatal(1, "Cannot open DMA file: %s", fname);
+
+	// wait until out of reset
+	wait (PRESETn == 1'b1);
+	wait (sys_reset == 1'b0);
+
+	forever
+		begin
+			// if EOF, rewind and continue
+			if ($feof(fd)) begin
+				$rewind(fd);
+			end
+
+			// read one vector
+			if ($fscanf(fd, "%h\n", vec) != 1)
+				$fatal(1, "Bad DMA data format");
+
+			// present data
+			@(posedge sys_clk);
+			dma_data  <= vec;
+			dma_valid <= 1'b1;
+
+			// wait for ack
+			while (dma_ack != 1'b1)
+				@(posedge sys_clk);
+
+			// drop valid after ack
+			@(posedge sys_clk);
+			dma_valid <= 1'b0;
+		end
+	end
+endtask
+endmodule   // anids_tb
 
