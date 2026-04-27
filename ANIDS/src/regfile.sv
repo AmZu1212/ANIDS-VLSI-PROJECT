c7388e220a7b1136edcf6d6f82132866715759ca
Amir Zuabi <zuabiamir123@gmail.com>
2026-04-27 14:07:40 +0300

    seperated registerfile prdata logic

--- ANIDS/src/regfile.sv
+++ ANIDS/src/regfile.sv
@@ -59,28 +59,31 @@ module regfile(
 			always @(posedge pclk or negedge presetN) begin
 				if (!presetN) begin
 					regfile[i] <= #1 {`APB_DATA_WIDTH{1'b0}};
-					// pready <= 1'b0;
 				end
 				else if (hw_wr_en && hw_wr_addr == i) begin
 					regfile[i] <= #1 hw_wr_data;
 				end
 				else if (apb_w && target == i) begin
 					regfile[i] <= #1 pwdata;
-					// pready <= 1'b1;
-				end
-				else if(apb_r && target == i) begin
-					prdata <= #1 regfile[i];
-					// pready <= 1'b1;
 				end
 			end
 		end
 	endgenerate
 
+	// APB read data logic
+	always @(posedge pclk or negedge presetN) begin
+		if (!presetN) begin
+			prdata <= #1 {`APB_DATA_WIDTH{1'b0}};
+		end
+		else if (apb_r && target < `REG_COUNT) begin
+			prdata <= #1 regfile[target];
+		end
+	end
+
 	// pready logic, instant ready.
 	always @(posedge pclk or negedge presetN) begin
 		if (!presetN) begin
 			pready <= #1 1'b0;
-			prdata <= #1 '0;
 		end else begin
 			pready <= #1 apb_w || apb_r;   // zero-wait slave
 		end
