6f64cae8ebd9a413646ef3db49f212597c2ab012
Amir Zuabi <zuabiamir123@gmail.com>
2026-04-29 00:23:43 +0300

    added timers + dont touch to TCL

--- load_design.tcl
+++ load_design.tcl
@@ -1,4 +1,4 @@
-# set core count
+# Set core count
 set_host_options -max_cores 8
 
 # Load custom SRAM library
@@ -9,8 +9,7 @@ set_app_var target_library [list tsl18fs120_typ.db spram8x256_cb_typ.db]
 set_app_var link_library   [list * tsl18fs120_typ.db spram8x256_cb_typ.db dw_foundation.sldb]
 
 
-# load RTLS manually
-
+# load RTLs manually
 set_app_var search_path [concat $search_path [list ANIDS-VLSI-PROJECT/ANIDS ANIDS-VLSI-PROJECT/ANIDS/src ANIDS-VLSI-PROJECT/ANIDS/src/core]]
 
 set analyze_t0 [clock seconds]
@@ -38,22 +37,29 @@ set elaborate_t0 [clock seconds]
 elaborate anids_top
 puts "Elaborate time: [expr {[clock seconds] - $elaborate_t0}] seconds"
 
-
-
-
-# Load saved design - optional for later...
-#read_ddc anids_top.ddc
-
 # Set top
 current_design anids_top
 
 # Link and check
+set link_t0 [clock seconds]
 link
+puts "Link time: [expr {[clock seconds] - $link_t0}] seconds"
+
+set check_design_t0 [clock seconds]
 check_design
+puts "Check design time: [expr {[clock seconds] - $check_design_t0}] seconds"
 
 # Create clk signal
 create_clock -name sys_clk -period 5 [get_ports sys_clk]
-#report_clocks
+# report_clocks
+
+# Protect SRAM macro instances
+set_dont_touch [get_cells -hier *function_lut*]
+puts "SRAM macro instances protected: [sizeof_collection [get_cells -hier *function_lut*]]"
+
+set write_ddc_t0 [clock seconds]
+write -format ddc -hierarchy -output anids_top_precompile.ddc
+puts "Write DDC time: [expr {[clock seconds] - $write_ddc_t0}] seconds"
 
 # Run Compile (takes logn so commented)
-#compile
+# compile
